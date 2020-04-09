e4b1861b7965dfad92f68f7f115030b223dad942
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-09 16:12:58 -0500

    completed ex. 1 of junit testing

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -24,4 +24,51 @@ public class StudentTest {
         assertEquals("Student Two", Student.name);
 //        assertNotNull(Student.grades);
     }
+
+    @Test
+    public void checkIdIsReturn() {
+        assertEquals(1, Student.getId());
+        Student.id = 3;
+        assertEquals(3,Student.getId());
+        Student.id = 4;
+        assertEquals(4, Student.getId());
+    }
+
+    @Test
+    public void checkNameIsReturn() {
+        assertEquals("Student One", Student.getName());
+        Student.name = "Student Five";
+        assertEquals("Student Five", Student.getName());
+        Student.name = "$%5Hello";
+        assertNotEquals("Student Five", Student.getName());
+        assertEquals("$%5Hello", Student.getName());
+    }
+
+    @Test
+    public void checkIfGradeCanBeAdded() {
+        assertEquals(0, Student.grades.size());
+        Student.addGrade(90);
+        assertEquals(1, Student.grades.size());
+        Student.addGrade(60);
+        assertEquals(2, Student.grades.size());
+        assertEquals(90, (long)Student.grades.get(0));
+        assertEquals(60, (long)Student.grades.get(1));
+    }
+
+    @Test
+    public void checkIfGradeAverageCorrect() {
+        Student.addGrade(90);
+        Student.addGrade(80);
+        Student.addGrade(100);
+        Student.addGrade(35);
+        assertNotEquals(83, Student.getGradeAverage(),0); //assertEquals would give a failed test
+        assertEquals(76.25, Student.getGradeAverage(), 0);
+
+        Student.grades.clear();
+        Student.addGrade(78);
+        assertEquals(78, Student.getGradeAverage(), 0);
+
+
+    }
+
 }
